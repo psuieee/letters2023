31df3074e949cef94ff494ba47f214090f3847e2
wymcg <willem.mcg@gmail.com>
2022-07-11 17:28:42 -0500

    Burn

--- include/LEDCommand.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-struct LEDCommand {
-    uint32_t col, row;
-    uint8_t r, g, b;
-};
--- src/Pattern.cpp
+++
@@ -1,9 +0,0 @@
-#include "Pattern.h"
-
-using namespace std::chrono;
-
-Pattern::Pattern() : Pattern(milliseconds(0)) {}
-
-Pattern::Pattern(milliseconds startMs) {
-    this->ms = startMs;
-}
