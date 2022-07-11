488bc45ab233d72878f85f157cbf0716eac7c790
wymcg <willem.mcg@gmail.com>
2022-07-11 18:58:04 -0500

    Undo

--- src/LightCore.cpp
+++ src/LightCore.cpp
@@ -13,8 +13,5 @@ LightCore::LightCore(uint16_t newWidth, uint16_t newHeight)
 }
 
 void LightCore::tick(milliseconds newMs) {
-  this->state.updateState({PixelState{0, 0, 255, 255, 255}});
-
-  interface.draw();
 
 }
