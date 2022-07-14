102e6b9d10514339598da1bea68a5a988a625fb4
wymcg <willem.mcg@gmail.com>
2022-07-13 23:38:00 -0500

    Added TPSCheck

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "patterns/TestPattern.h"
 #include "patterns/AllWhite.h"
 #include "patterns/TVStatic.h"
+#include "patterns/TPSCheck.h"
 
 #define LC_WIDTH    32
 #define LC_HEIGHT   32
@@ -19,6 +20,7 @@ using namespace std::chrono;
 int main(int argc, char **argv) {
 
     // declare patterns
+    TPSCheck tc(LC_WIDTH, LC_HEIGHT);
     TestPattern tp(LC_WIDTH, LC_HEIGHT);
     AllWhite aw(LC_WIDTH, LC_HEIGHT);
     TVStatic st(LC_WIDTH, LC_HEIGHT);
@@ -27,6 +29,7 @@ int main(int argc, char **argv) {
     LightCore lc(LC_WIDTH, LC_HEIGHT, LC_TPS, LC_MS_PATT);
 
     // add the patterns
+    lc.addPattern(&tc);
     lc.addPattern(&tp);
     lc.addPattern(&aw);
     lc.addPattern(&st);
