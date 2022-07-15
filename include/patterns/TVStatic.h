0be356cd6ee952adee78543954d849c0d54555b0
wymcg <willem.mcg@gmail.com>
2022-07-14 21:03:04 -0500

    Performance improvements for TVStatic

--- include/patterns/TVStatic.h
+++ include/patterns/TVStatic.h
@@ -6,33 +6,39 @@
 
 class TVStatic : public Pattern {
 public:
-    TVStatic(uint16_t newWidth, uint16_t newHeight) : Pattern(newWidth, newHeight) {};
-
-    vector<PixelState> getUpdates() {
-        vector<PixelState> updates;
-        PixelState p;
-        
+    TVStatic(uint16_t newWidth, uint16_t newHeight) : Pattern(newWidth, newHeight) {
+        this->updates = vector<PixelState>(newWidth * newHeight, PixelState{});
         for (int y = 0; y < this->height; y++) {
             for (int x = 0; x < this->width; x++) {
-                PixelState p;
+                uint32_t fullTarget = (y * this->height) + x;
 
-                p.x = x;
-                p.y = y;
+                this->updates[fullTarget].x = x;
+                this->updates[fullTarget].y = y;
+            }
+        }
+    };
+
+    vector<PixelState> getUpdates() {
+        uint32_t fullTarget;
+
+        for (int y = 0; y < this->height; y++) {
+            for (int x = 0; x < this->width; x++) {
+                fullTarget = (y * this->height) + x;
 
                 if ((rand() % 2) == 0) {
-                    p.color.r = 255;
-                    p.color.g = 255;
-                    p.color.b = 255;
+                    this->updates[fullTarget].color = white;
                 } else {
-                    p.color.r = 0;
-                    p.color.g = 0;
-                    p.color.b = 0;
+                    this->updates[fullTarget].color = black;
                 }
-
-                updates.push_back(p);
+                
             }
         }
 
         return updates;
     };
+
+private:
+    Color white = Color(255, 255, 255);
+    Color black = Color(0, 0, 0);
+    vector<PixelState> updates;
 };
