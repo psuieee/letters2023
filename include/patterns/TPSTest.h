3c827bdb91583ade6ff59de1745457e6096f2470
wymcg <willem.mcg@gmail.com>
2022-07-17 14:25:09 -0500

    TPSTest changes

--- include/patterns/TPSTest.h
+++ include/patterns/TPSTest.h
@@ -5,40 +5,41 @@
 class TPSTest : public Pattern {
 public:
     TPSTest(uint16_t newWidth, uint16_t newHeight) : Pattern(newWidth, newHeight) {
-        this->state = (Color *) calloc(this->height * this->width, sizeof(Color(0, 0, 0)));
+        this->leftwhitestate = (Color *) calloc(this->height * this->width, sizeof(Color(0, 0, 0)));
+        this->rightwhitestate = (Color *) calloc(this->height * this->width, sizeof(Color(0, 0, 0)));
+
+        for (int y = 0; y < this->height; y++) {
+            for (int x = 0; x < (this->width / 2); x++) {
+                this->leftwhitestate[y * this->height + x] = white;
+            }
+            for (int x = (this->width / 2); x < this->width; x++) {
+                this->leftwhitestate[y * this->height + x] = black;
+            }
+        }
+
+        for (int y = 0; y < this->height; y++) {
+            for (int x = 0; x < (this->width / 2); x++) {
+                this->rightwhitestate[y * this->height + x] = black;
+            }
+            for (int x = (this->width / 2); x < this->width; x++) {
+                this->rightwhitestate[y * this->height + x] = white;
+            }
+        }
     };
 
     Color * getUpdates() {
         this->leftWhite = !this->leftWhite;
 
         if (leftWhite) {
-            #pragma omp parallel for
-            for (int y = 0; y < this->height; y++) {
-                for (int x = 0; x < (this->width / 2); x++) {
-                    this->state[y * this->height + x] = white;
-                }
-                for (int x = (this->width / 2); x < this->width; x++) {
-                    this->state[y * this->height + x] = black;
-                }
-            }
+            return leftwhitestate;
         } else {
-            #pragma omp parallel for
-            for (int y = 0; y < this->height; y++) {
-                for (int x = 0; x < (this->width / 2); x++) {
-                    this->state[y * this->height + x] = black;
-                }
-                for (int x = (this->width / 2); x < this->width; x++) {
-                    this->state[y * this->height + x] = white;
-                }
-            }
+            return rightwhitestate;
         }
-
-        return this->state;
     };
 
 private:
     Color black = Color(0, 0, 0);
     Color white = Color(255, 255, 255);
     bool leftWhite = false;
-    Color *state;
+    Color *leftwhitestate, *rightwhitestate;
 };
